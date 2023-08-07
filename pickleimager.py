70c1486ab948695e9ad348cdf88f89bcff516de6
Christopher Paolini <paolini@engineering.sdsu.edu>
2023-08-07 16:44:57 -0700

    Fix aspect ratio

--- pickleimager.py
+++ pickleimager.py
@@ -20,7 +20,7 @@ y_test = pickle.load(file)
 rows = X_train.shape[0]
 for row in range(0, rows):
     a = np.reshape(X_train[row], (1024, 2048))
-    fig = plt.figure(figsize=(3,6))
+    fig = plt.figure(figsize=(6,3))
     fig.suptitle(f"minDistSph: {y_train[row][0]}", fontsize=16)
     ax = fig.add_subplot(111)
     ax.pcolor(a,cmap='hot')
