66a76e2dd4d1db856e650a3d80b4c04a904842a3
janagonzlz <158417215+janagonzlz@users.noreply.github.com>
2024-06-13 14:20:00 -0700

    Code updates.

--- patches.py
+++ patches.py
@@ -21,89 +21,126 @@ if __name__ == '__main__':
             sys.exit()
         else:
             os.chdir(path)
+            
+path = '/Users/janagonzalez/chombocrunch/EBAMR/EBAMRINS/execPAMR/'
+pFile = re.compile('plot.nx128.step(\d+).2d.hdf5')
+pLevel = re.compile('level_(\d+)')
+a = np.empty([0,7])
 
-    pFile = re.compile(r'plot.nx128.step(\d+).2d.hdf5')
-    pLevel = re.compile(r'level_(\d+)')
+i = 0
+for file in listdir(path):
+    mFile = pFile.match(file)
+
+    if mFile:
+        stepNum = int(mFile.group(1))
+        print(f'file: {file}, step: {stepNum}')
+        hf_in = h5py.File(file, "r")
+        root = hf_in["/"]
+
+        attributes = hf_in["level_0/data_attributes/"]
+        data = hf_in["level_0/data:datatype=0"]
+        componentKeys = list(root.attrs.keys())
+        comps = attributes.attrs["comps"]
+        components = [i.decode('utf-8') if isinstance(i, np.bytes_) else '' for i in list(root.attrs.values())]
+        dataNumPy = np.array(data, np.float64)
+        
+        dxArray = np.empty((2,))
+     
+        isPrinted = False
+        for key in root.keys():
+            #print(f'\nkey: {key}', end=' ')
+            mLevel = pLevel.match(key)
+            #dataNumPy = np.array(data, np.float64)
+            
+            if mLevel:
+                levelNum = int(mLevel.group(1))
+                #print(f'level number: {levelNum}', end=' ')         # printing out each level number 
+                levelHandle = hf_in[mLevel.group(0) + "/"]           # Making title 
+                time = levelHandle.attrs["time"]
+                level = hf_in[mLevel.group(0)]                
                 
-    a = np.empty([0,7])
-    for file in listdir(path):
-        #print(file)
-        mFile = pFile.match(file)
-
-        if mFile:
-            stepNum = int(mFile.group(1))
-            print(f'file: {file}, step: {stepNum}')
-            hf_in = h5py.File(file, "r")
-            root = hf_in["/"]
-
-            dxArray = np.empty((2,))
-
-            isPrinted = False
-            for key in root.keys():
-                #print(f'\nkey: {key}', end=' ')
-                mLevel = pLevel.match(key)
+                dx = level.attrs["dx"]                               # Printing dx value 
+                dxArray[levelNum] = dx
+                #print(f'dx: {dx}', end=' ')
                 
-                if mLevel:
-                    levelNum = int(mLevel.group(1))
-                    #print(f'level number: {levelNum}', end=' ')              # printing out each level number 
-                    levelHandle = hf_in[mLevel.group(0) + "/"]           # Making title 
-                    time = levelHandle.attrs["time"]
-                    level = hf_in[mLevel.group(0)]                
-                    dx = level.attrs["dx"]                          # Printing dx value 
-                    dxArray[levelNum] = dx
-                    #print(f'dx: {dx}', end=' ')
-                    boxes = hf_in[mLevel.group(0) + "/boxes"]
-                    data_attributes = hf_in[mLevel.group(0) + "/data_attributes"]
-                    numBoxes = boxes.shape[0]
-                    #print(f'number of boxes: {numBoxes}', end=' ')
+                boxes = hf_in[mLevel.group(0) + "/boxes"]
+                data_attributes = hf_in[mLevel.group(0) + "/data_attributes"]
+                numBoxes = boxes.shape[0]
+                #print(f'number of boxes: {numBoxes}', end=' ')
 
-                    # get velocity data here
+                if levelNum == 0:
                     prob_domain = level.attrs["prob_domain"]
-                    X, Y = np.mgrid[prob_domain[1]:prob_domain[3]+1, prob_domain[0]:prob_domain[2]+1]
-                    velocity0 = np.zeros(X.shape)
-
-                    i = 0
-                    for box in boxes:
-
-                        #if levelNum > 0:
-                        #    box = (box[0], box[1], box[2], box[3])                     
-                        
-                        # each box is a 4-tuple
-                        X = (box[0] * dx, box[1] * dx), (box[2] - box[0] + 1) * dx
-                        Y = (box[3] - box[1] + 1) * dx
-
-                        #boxDim = (box[2] - box[0] + 1, box[3] - box[1] + 1)
-                        # print(f'box {i}: {boxDim}, box : {box}', end=' ')
+                    X, Y= np.mgrid[prob_domain[1]:prob_domain[3]+1, prob_domain[0]:prob_domain[2]+1]
                 
-                        width = box[0] * dx, box[1] * dx, (box[2] - box[0] + 1) * dx
-                        height =  (box[3] - box[1] + 1) * dx
-                        boxDim = (X, Y)
+                    # Box dimensions------------------------------------
+                    boxDim = (boxes[0][2] - boxes[0][0] + 1, boxes[0][3] - boxes[0][1] + 1) # assume level 0 boxes are all of equivalent dimension
+                    nBoxes = boxes.shape[0]
+                    nRows = X.shape[0]
+                    nCols = X.shape[1]
+                    patchRows = int(nRows/boxDim[1])
+                    patchCols = int(nCols/boxDim[0])
 
-                        v = velocity0[box[1]:box[3]+1,box[0]:box[2]+1]
-                        stdev = np.std(v)
+                    boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
+                
+                    # Velocity extraction------------------------------------
+                    velocity0_i = int(re.findall(r'\d+', componentKeys[components.index("velocity0")])[0]) 
 
-                        #print(f'box {boxDim}, origin box dim : {box}', end=' ')
-                        l = [stepNum, levelNum, box[0] * dx, box[1] * dx,  (box[2] - box[0] + 1) * dx, (box[3] - box[1] + 1) * dx, stdev]
-                        
+                    velocity0 = np.zeros(X.shape)
+                    #print(f"velocity0.shape {velocity0.shape}")
+                
+                    #print(f"patchCols {patchCols}")
+                    for col in range(patchCols):
+                        #print(f"patchRows {patchRows}")
+                        for row in range(patchRows):
+                         #   print(f"[{row * boxDim[0]} : {(row + 1) * boxDim[0]}, {col * boxDim[1]} : {(col + 1) * boxDim[1]}]")
+                            velocity0[row * boxDim[0] : (row + 1) * boxDim[0], col * boxDim[1] : (col + 1) * boxDim[1]] = \
+                                np.lib.stride_tricks.as_strided(boxData[col * patchRows + row, velocity0_i*(boxDim[0] * boxDim[1]):(velocity0_i+1)*(boxDim[0] * boxDim[1])], \
+                                shape=(boxDim[0],boxDim[1]),\
+                                strides=(8 * boxDim[1], 8 * 1))               
+
+                i = 0
+                for box in boxes:
+    
+                    boxDim = (box[2] - box[0] + 1, box[3] - box[1] + 1)                
+                    
+                    # each box is a 4-tuple
+                    X = (box[0] * dx, box[1] * dx), (box[2] - box[0] + 1) * dx
+                    Y = (box[3] - box[1] + 1) * dx
+
+                    # print(f'box {i}: {boxDim}, box : {box}', end=' ')
+
+                    width = box[0] * dx, box[1] * dx, (box[2] - box[0] + 1) * dx
+                    height = (box[3] - box[1] + 1) * dx
+                    
+                    comps == int(data.shape[0]/((boxDim[0] * boxDim[1]) * nBoxes ))
+                    dataNumPy.shape[0]/nBoxes == boxDim[0] * boxDim[1] * comps
+
+
+                    boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
+
+                    v = velocity0[box[1]:box[3]+1,box[0]:box[2]+1]
+                    #print(f"v {v.shape}: {v}")
+                    #print(np.max(v))
+                    stdev = np.std(v)
+
+                    if math.isnan(stdev):
+                        print(f'nan v : {v}, v dim: {v.shape}')  
+                        print(f"level: {levelNum}")
                         print(l)
+                        print(f"v {v.shape}: {v}\n\n\n")
+                        #sys.exit(1)
 
-                        a = np.append(a, [l], axis=0)
-                        if isPrinted == False:
-                            isPrinted = True
-                            print(l)
-                            #print(a[-1,:])
-                        
-                        i +=1
+                    #print(f'box {boxDim}, origin box dim : {box}', end=' ')
+                    l = [stepNum, levelNum, box[0] * dx, box[1] * dx,  (box[2] - box[0] + 1) * dx, (box[3] - box[1] + 1) * dx, stdev]
+                    
+                    #print(l)
 
-            #print('\n')
-    #print(a)
-    #print(a.shape)
-    file = 'patches.pkl'
-    with open(file, 'wb') as f:
-        pickle.dump(a, f)
-    if os.path.exists(file):
-        print (f'pickle file: {file}')
-        file_stats = os.stat(file)
-        print(f'File Size in Bytes is {file_stats.st_size}')
-    else:
-        print (f'failed to generate pickle file: {file}')
+                    a = np.append(a, [l], axis=0)
+                    if isPrinted == False:
+                        isPrinted = True
+                        print(l)
+                    
+                    i +=1
+
+with open('patches.pkl', 'wb') as f:
+    pickle.dump(a, f)
