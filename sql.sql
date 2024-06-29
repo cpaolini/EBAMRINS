0b5b61aa4e468cbb66d303da84ee38228d17a607
janagonzlz <158417215+janagonzlz@users.noreply.github.com>
2024-06-28 17:42:21 -0700

    SQL code implementation

--- sql.sql
+++ sql.sql
@@ -1,7 +1,12 @@
 --CREATE TABLE patch_database (step INTEGER NOT NULL, X REAL NOT NULL,Y REAL NOT NULL, 
 --Probability REAL NOT NULL, Width REAL NOT NULL, Height REAL NOT NULL);
 
-INSERT INTO patch_database (step, X, Y, Probability, Width, Height) 
-VALUES (step, {A[origin,0]}, {A[origin,1]}, ZZ, xx, yy);
+ALTER TABLE patch_database
+DROP COLUMN Width;
 
--- SELECT * FROM patch_database;
+
+
+-- INSERT INTO patch_database (step, X, Y, Probability, Width, Height) 
+-- VALUES (step, {A[origin,0]}, {A[origin,1]}, ZZ, xx, yy);
+
+-- -- SELECT * FROM patch_database;
