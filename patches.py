cb87421ce015ca76673248f082772d871a9fd7be
Christopher Paolini <paolini@engineering.sdsu.edu>
2025-07-06 12:57:39 -0700

    Updates.

--- patches.py
+++ patches.py
@@ -5,6 +5,7 @@ import math
 import re
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
+from numpy import linalg as LA
 import pickle
 from os import listdir
 from os.path import isfile, join
@@ -32,8 +33,9 @@ if __name__ == '__main__':
             
 pFile = re.compile('plot.nx128.step(\d+).2d.hdf5')
 pLevel = re.compile('level_(\d+)')
-a = np.empty([0,8])
-
+a = np.empty([0,9])
+np.set_printoptions(legacy='1.25')
+np.set_printoptions(linewidth=np.inf) 
 
 #i = 0
 
@@ -47,43 +49,64 @@ for file in sorted_directory(path):
         hf_in = h5py.File(file, "r")
         root = hf_in["/"]
 
-        # attributes = hf_in["level_0/data_attributes/"]
-        # data = hf_in["level_0/data:datatype=0"]
-        # componentKeys = list(root.attrs.keys())
-        # comps = attributes.attrs["comps"]
-        # components = [i.decode('utf-8') if isinstance(i, np.bytes_) else '' for i in list(root.attrs.values())]
-        # dataNumPy = np.array(data, np.float64)
+        attributes = hf_in["level_0/data_attributes/"]
+        data = hf_in["level_0/data:datatype=0"]
+        componentKeys = list(root.attrs.keys())
+        comps = attributes.attrs["comps"]
+        components = [i.decode('utf-8') if isinstance(i, np.bytes_) else '' for i in list(root.attrs.values())]
+        dataNumPy = np.array(data, np.float64)
         
-        #level_0 = hf_in["level_0/"]
-        #prob_domain = level_0.attrs["prob_domain"]
-        #X, Y = np.mgrid[prob_domain[1]:prob_domain[3]+1, prob_domain[0]:prob_domain[2]+1]
+        level_0 = hf_in["level_0/"]
+        prob_domain = level_0.attrs["prob_domain"]
+        X, Y = np.mgrid[prob_domain[1]:prob_domain[3]+1, prob_domain[0]:prob_domain[2]+1]
                 
         # Box dimensions
-        #boxes = hf_in["level_0/boxes"]
-        # boxDim = (boxes[0][2] - boxes[0][0] + 1, boxes[0][3] - boxes[0][1] + 1) # assume level 0 boxes are all of equivalent dimension
-        # nBoxes = boxes.shape[0]
-        # nRows = X.shape[0]
-        # nCols = X.shape[1]
-        # patchRows = int(nRows/boxDim[1])
-        # patchCols = int(nCols/boxDim[0])
-        # boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
+        boxes = hf_in["level_0/boxes"]
+        boxDim = (boxes[0][2] - boxes[0][0] + 1, boxes[0][3] - boxes[0][1] + 1) # assume level 0 boxes are all of equivalent dimension
+        nBoxes = boxes.shape[0]
+        nRows = X.shape[0]
+        nCols = X.shape[1]
+        patchRows = int(nRows/boxDim[1])
+        patchCols = int(nCols/boxDim[0])
+        boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
                 
-        # # Velocity extraction
-        # velocity0_i = int(re.findall(r'\d+', componentKeys[components.index("velocity0")])[0]) 
+        # Velocity extraction
+        velocity0_i = int(re.findall(r'\d+', componentKeys[components.index("velocity0")])[0]) 
+        #print(f'velocity0 index: {velocity0_i}')
+        velocity1_i = int(re.findall(r'\d+', componentKeys[components.index("velocity1")])[0]) 
+        #print(f'velocity1 index: {velocity1_i}')
 
-        # velocity0 = np.zeros(X.shape)
-        # #print(f"velocity0.shape {velocity0.shape}")
+        vorticity_i = int(re.findall(r'\d+', componentKeys[components.index("vorticity")])[0]) 
+        #print(f'vorticity index: {vorticity_i}')
+
+        velocity0 = np.zeros(X.shape)
+        #print(f"velocity0.shape {velocity0.shape}")
+        velocity1 = np.zeros(X.shape)
+        #print(f"velocity1.shape {velocity1.shape}")
                 
-        # #print(f"patchCols {patchCols}")
-        # for col in range(patchCols):
-        #     #print(f"patchRows {patchRows}")
-        #     for row in range(patchRows):
-        #         #   print(f"[{row * boxDim[0]} : {(row + 1) * boxDim[0]}, {col * boxDim[1]} : {(col + 1) * boxDim[1]}]")
-        #         velocity0[row * boxDim[0] : (row + 1) * boxDim[0], col * boxDim[1] : (col + 1) * boxDim[1]] = \
-        #             np.lib.stride_tricks.as_strided(boxData[col * patchRows + row, velocity0_i*(boxDim[0] * boxDim[1]):(velocity0_i+1)*(boxDim[0] * boxDim[1])], \
-        #             shape=(boxDim[0],boxDim[1]),\
-        #             strides=(8 * boxDim[1], 8 * 1))   
-        
+        vorticity = np.zeros(X.shape)
+        #print(f"vorticity.shape {vorticity.shape}")
+
+        #print(f"patchCols {patchCols}")
+        for col in range(patchCols):
+            #print(f"patchRows {patchRows}")
+            for row in range(patchRows):
+                #   print(f"[{row * boxDim[0]} : {(row + 1) * boxDim[0]}, {col * boxDim[1]} : {(col + 1) * boxDim[1]}]")
+                velocity0[row * boxDim[0] : (row + 1) * boxDim[0], col * boxDim[1] : (col + 1) * boxDim[1]] = \
+                    np.lib.stride_tricks.as_strided(boxData[col * patchRows + row, velocity0_i*(boxDim[0] * boxDim[1]):(velocity0_i+1)*(boxDim[0] * boxDim[1])], \
+                    shape=(boxDim[0],boxDim[1]),\
+                    strides=(8 * boxDim[1], 8 * 1))  
+                velocity1[row * boxDim[0] : (row + 1) * boxDim[0], col * boxDim[1] : (col + 1) * boxDim[1]] = \
+                    np.lib.stride_tricks.as_strided(boxData[col * patchRows + row, velocity1_i*(boxDim[0] * boxDim[1]):(velocity1_i+1)*(boxDim[0] * boxDim[1])], \
+                    shape=(boxDim[0],boxDim[1]),\
+                    strides=(8 * boxDim[1], 8 * 1))   
+                vorticity[row * boxDim[0] : (row + 1) * boxDim[0], col * boxDim[1] : (col + 1) * boxDim[1]] = \
+                    np.lib.stride_tricks.as_strided(boxData[col * patchRows + row, vorticity_i*(boxDim[0] * boxDim[1]):(vorticity_i+1)*(boxDim[0] * boxDim[1])], \
+                    shape=(boxDim[0],boxDim[1]),\
+                    strides=(8 * boxDim[1], 8 * 1))
+
+        #exit()
+
         dxArray = np.empty((0,))
         for key in root.keys():
             #print(f'\nkey: {key}', end=' ')
@@ -110,21 +133,30 @@ for file in sorted_directory(path):
 
                 #i_box = 0                       
                 for box in boxes:
-                    # boxDim = (box[2] - box[0] + 1, box[3] - box[1] + 1)                
-                    
-                    #print(f'box {i}: {boxDim}, box : {box}', end=' ')
+                    boxDim = (box[2] - box[0] + 1, box[3] - box[1] + 1)
+                    width = (box[2] - box[0] + 1) * dx
+                    height = (box[3] - box[1] + 1) * dx
+                    #print(f'box: {boxDim}, {box}, width: {width}, height: {height}')
 
-                    # width = box[0] * dx, box[1] * dx, (box[2] - box[0] + 1) * dx
-                    # height = (box[3] - box[1] + 1) * dx
+                    comps == int(data.shape[0]/((boxDim[0] * boxDim[1]) * nBoxes ))
+                    dataNumPy.shape[0]/nBoxes == boxDim[0] * boxDim[1] * comps
+                    boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
+                    factor = 2**levelNum
+                    vx = velocity0[box[1]//factor:(box[3]//factor)+1,box[0]//factor:(box[2]//factor)+1]
+                    #print(f"vx {vx.shape}: {vx}")
+                    vy = velocity1[box[1]//factor:(box[3]//factor)+1,box[0]//factor:(box[2]//factor)+1]
+                    #print(f"vy {vy.shape}: {vy}")
+
+                    #cv = vorticity[box[1]//factor:(box[3]//factor)+1,box[0]//factor:(box[2]//factor)+1]
+                    #print(f"cv {cv.shape}: {cv}")
+
+                    vmag = math.sqrt(LA.norm(vx) ** 2 + LA.norm(vy) ** 2)
+                    #print(f'|v|: {vmag}')
+
+                    #exit()
                     
-                    # comps == int(data.shape[0]/((boxDim[0] * boxDim[1]) * nBoxes ))
-                    # dataNumPy.shape[0]/nBoxes == boxDim[0] * boxDim[1] * comps
-                    # boxData = dataNumPy.reshape((nBoxes,int(dataNumPy.shape[0]/nBoxes)))
-                    # factor = 2**levelNum
-                    # v = velocity0[box[1]//factor:(box[3]//factor)+1,box[0]//factor:(box[2]//factor)+1]
-                    #print(f"v {v.shape}: {v}")
                     #print(np.max(v))
-                    # stdev = np.std(v) 
+                    #stdev = np.std(v) 
 
                     # if math.isnan(stdev):
                     #     print(f'nan v : {v}, v dim: {v.shape}, {box[1]}:{box[3]+1},{box[0]}:{box[2]+1}')  
@@ -134,8 +166,8 @@ for file in sorted_directory(path):
                     #     sys.exit(1)
 
                     #print(f'box {boxDim}, origin box dim : {box}', end=' ')
-                    l = [stepNum, levelNum, time, box[0], box[1],  (box[2] - box[0] + 1), (box[3] - box[1] + 1), dx]
-                    print(l)
+                    l = [stepNum, levelNum, time, box[0], box[1],  (box[2] - box[0] + 1), (box[3] - box[1] + 1), dx, vmag]
+                    #print(l)
                     a = np.append(a, [l], axis=0)      
                     
                     #i_box =+ 1
